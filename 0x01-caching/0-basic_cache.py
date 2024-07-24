de7028b97ac7d7c91af8ce6fe9458975f6b9fa52
michael-shambel <Michaelshambel10@gmail.com>
2024-07-24 17:06:16 +0300

    task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+class BasicCache that inherits from BaseVaching
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    creating a method put and get that do some specific tasks
+    """
+    def put(self, key, item):
+        """
+        this method put key and item in the dictionary
+        """
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        this method get item by the key
+        """
+        if key is None and key not in self.cache_data:
+            return None
+        return self.cache_data[key]
