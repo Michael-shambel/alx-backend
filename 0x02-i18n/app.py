b182c66120434b5e29f870501c206c8c8b2e4af2
michael-shambel <Michaelshambel10@gmail.com>
2024-07-31 00:57:38 +0300

    task 100

---
+++ 0x02-i18n/app.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+"""
+Based on the inferred time zone, display the current time on the
+home page in the default format
+"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, _, lazy_gettext as _l
+import pytz
+from pytz import UnknownTimeZoneError
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+class Config:
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    user_id = request.args.get('login_as')
+    if user_id:
+        user = users.get(int(user_id))
+        return user
+    return None
+
+
+@app.before_request
+def before_request():
+    g.user = get_user()
+
+
+@babel.localeselector
+def get_locale():
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    if g.user and g.user['locale'] in app.config['LANGUAGES']:
+        return g.user['locale']
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@babel.timezoneselector
+def get_timezone():
+    timezone = request.args.get('timezone')
+    if timezone:
+        try:
+            pytz.timezone(timezone)
+            return timezone
+        except UnknownTimeZoneError:
+            pass
+    if g.user and g.user['timezone']:
+        try:
+            pytz.timezone(g.user['timezone'])
+            return g.user['timezone']
+        except UnknownTimeZoneError:
+            pass
+    return app.config['BABEL_DEFAULT_TIMEZONE']
+
+
+@app.route('/')
+def index():
+    current_time = datetime.now(pytz.timezone(get_timezone()))
+    formatted_time = current_time.strftime('%c')
+    return render_template('6-index.html', current_time=formatted_time)
+
+
+if __name__ == '__main__':
+    app.run()
