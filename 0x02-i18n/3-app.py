fbbe69110c4da53b5e6d716d3a40859fa490748b
michael-shambel <Michaelshambel10@gmail.com>
2024-07-30 21:33:54 +0300

    task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+the _ or gettext function to parametrize your templates. Use the message IDs
+home_title and home_header.
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config:
+    """Configure available languages in our app."""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Determine the best match with our supported languages."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def home():
+    """Render the home page."""
+    return render_template('3-index.html', locale=get_locale())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
