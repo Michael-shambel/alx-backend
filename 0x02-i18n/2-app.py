1d4c3dbdcb087ca50885165f3198f2c0f9c1edbe
michael-shambel <Michaelshambel10@gmail.com>
2024-07-30 19:37:40 +0300

    task 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """
-
+get_locale function with the babel.localeselector decorator.
+Use request.accept_languages to determine the best match with
+our supported languages.
 """
 from flask import Flask, render_template, request
 from flask_babel import Babel
@@ -21,11 +23,15 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
+    """Get locale from request.accept
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index():
+    """Return 2-index.html
+    """
     return render_template('2-index.html', locale=get_locale())
 
 
