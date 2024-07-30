b2e9c701e83d1b8c220544bfdef0101d8470a8d6
michael-shambel <Michaelshambel10@gmail.com>
2024-07-30 18:51:04 +0300

    task 0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+setup a basic flask app and create a single route that simply out put
+"Welcome to Holberton" as page title and "Hello World" as header
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('0-index.html')
