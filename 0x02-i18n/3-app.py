8153814ead74c0d314df6d630dbf23ec125b0c8a
michael-shambel <Michaelshambel10@gmail.com>
2024-08-02 16:54:21 +0300

    task 5

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -23,7 +23,7 @@ babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """
     Determine the best match with our supported languages.
     """
@@ -31,7 +31,7 @@ def get_locale():
 
 
 @app.route('/')
-def home():
+def home() -> str:
     """
     Render the home page.
     """
